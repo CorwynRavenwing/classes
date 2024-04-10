63540cd8fbbc6cc75474830d4b4f1e974c74e955
Warren Harmon <oakenraven@juno.com>
2024-04-09 21:47:44 -0400

    updated cut the tree

--- python/hackerrank/algorithms/cut_the_tree.py
+++ python/hackerrank/algorithms/cut_the_tree.py
@@ -14,61 +14,63 @@ def cutTheTree(data, edges):
     total_tree_value = sum(data)
     print(f"#{total_tree_value=}")
     answers = []
-    print(f"#{edges=}")
-    print(f"#{list(zip(*edges))=}")
-    print(f"#{list(map(list, zip(*edges)))=}")
-    E = list(map(list, zip(*edges)))
-    endpoints = E[0] + E[1]
-    print(f"#{endpoints=}")
-    endpoint_counter = Counter(endpoints)
-    print(f"#{endpoint_counter=}")
-    leaf_nodes = [
-        node
-        for node, count in endpoint_counter.items()
-        if count == 1
-    ]
-    print(f"#{leaf_nodes=}")
-    leaf_edges = {
-        node: (A, B)
-        for node in leaf_nodes
-        for A, B in edges
-        if A == node or B == node
-    }
-    print(f"#{leaf_edges=}")
-
-    exit()
-
-    pass
-
-    for edge in edges:
-        print(f"#{edge=}")
-        edges_test = edges.copy()
-        edges_test.remove(edge)
-        group = []
-        check = [1]
-        while check:
-            group.extend(check)
-            check = [
-                B
-                for A, B in edges_test
-                if A in check and B not in group
-            ] + [
-                A
-                for A, B in edges_test
-                if B in check and A not in group
-            ]
-            print(f"#{len(group)=} {len(check)=}")
-        value_without_edge = [
-            data[node-1]
-            for node in group
+    while edges:
+        print(f"#edges={len(edges)}")
+        # print(f"#{list(zip(*edges))[:10]=}")
+        # print(f"#{list(map(list, zip(*edges)))[:10]=}")
+        E = list(map(list, zip(*edges)))
+        endpoints = E[0] + E[1]
+        # print(f"#{endpoints[:10]=}")
+        endpoint_counter = Counter(endpoints)
+        # print(f"#{tuple(endpoint_counter)[:10]=}")
+        leaf_nodes = [
+            node
+            for node, count in endpoint_counter.items()
+            if count == 1
         ]
-        value_without_edge = sum(value_without_edge)
-        print(f"#{value_without_edge=}")
-        other_half = total_tree_value - value_without_edge
-        answers.append(
-            abs(other_half - value_without_edge)
-        )
-    print(f"#{answers=}")
+        print(f"#\tleaf_nodes={len(leaf_nodes)}")
+        leaf_edges = {
+            node: edge
+            for node in leaf_nodes
+            for edge in edges
+            if node in edge
+        }
+        # print(f"#{list(leaf_edges)[:10]=}")
+        # print(f"# B ##{edges[:10]=}")
+
+        # PASS 1: every leaf edge -> answers
+        for node, edge in leaf_edges.items():
+            value = data[node-1]
+            other_half = total_tree_value - value
+            answers.append(
+                abs(other_half - value)
+            )
+
+        # PASS 2: leaf edge data -> other node
+        for node, edge in leaf_edges.items():
+            (A, B) = edge
+            other_node = (
+                A if node == B else B
+            )
+            # print(f"#{node=} {other_node=} {edge=}")
+            # print(
+            #     f"#data {data[node-1]} ",
+            #     f"{data[other_node-1]}", end=" -> "
+            # )
+            data[other_node-1] += data[node-1]
+            data[node-1] = 0
+            # print(
+            #     f"# {data[node-1]} ",
+            #     f"{data[other_node-1]}"
+            # )
+
+        # pass 3: delete edge
+        for node, edge in leaf_edges.items():
+            if edge in edges:
+                # last pair will both be leaf nodes
+                # can't delete edge twice
+                edges.remove(edge)
+
     return min(answers)
 
 if __name__ == '__main__':
