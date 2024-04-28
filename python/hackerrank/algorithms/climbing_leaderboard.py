04c332ba7ced69b80724342fbbef5d9e0923af1b
Warren Harmon <oakenraven@juno.com>
2024-04-28 19:37:31 -0400

    faster climbing_leaderboard

--- python/hackerrank/algorithms/climbing_leaderboard.py
+++ python/hackerrank/algorithms/climbing_leaderboard.py
@@ -8,15 +8,25 @@ import os
 
 def climbingLeaderboard(ranked, player):
     retval = []
-    print(f"#{ranked=}")
+    print(f"#{ranked[:10]=}")
     nodups = set(ranked)
+    ranked_nodups = list(nodups)
+    ranked_nodups.sort(reverse=True)
+    print(f"#{ranked_nodups[:10]=}")
     for P in player:
-        nodups.add(P)
-        ranked_nodups = list(nodups)
-        ranked_nodups.sort(reverse=True)
+        if P not in ranked_nodups:
+            nearest_value = min(
+                ranked_nodups,
+                key=lambda x: abs(x - P)
+            )
+            print(f"#  {P=} {nearest_value=}")
+            index = ranked_nodups.index(nearest_value)
+            if P < nearest_value:
+                index += 1
+            ranked_nodups.insert(index, P)
+            print(f"#  {index=} {ranked_nodups[:10]=}")
         rank = ranked_nodups.index(P) + 1
-        # print(f"#  {rank=} RND={ranked_nodups}")
-        print(f"#  {rank=} LEN={len(nodups)}")
+        print(f"#  {P=} {rank=} LEN={len(ranked_nodups)}")
         retval.append(rank)
     return retval
 
