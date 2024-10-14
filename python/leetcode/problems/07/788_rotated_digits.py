c74bb13052437bc5132751a138c1a1368f9331db
Warren Harmon <oakenraven@juno.com>
2024-10-14 12:09:54 -0400

    add cache to leetcode

--- python/leetcode/problems/07/788_rotated_digits.py
+++ python/leetcode/problems/07/788_rotated_digits.py
@@ -19,17 +19,22 @@ class Solution:
         return (10 ** n) - 1
         # e.g. 3 digits -> 10^3 -> 1000 -> 999
 
+    @cache
     def numbersContaining_0125689(self, n: int) -> int:
+        DEBUG = False
         # these are numbers that can rotate, but may not be legal answers
-        print(f'numbersContaining_0125689({n}):')
+        if DEBUG: print(f'numbersContaining_0125689({n}):')
         answer = 0
         digits = self.digitCount(n)
         if digits == 1:
             first_digit = n     # because it's only one digit
             for d in range(first_digit + 1):
-            # d is every number LESS THAN OR EQUAL TO first_digit
+                # d is every number LESS THAN OR EQUAL TO first_digit
                 if d in [0, 1, 2, 5, 6, 8, 9]:
+                    if DEBUG: print(f'  {n}:{d} +1')
                     answer += 1
+                else:
+                    if DEBUG: print(f'  {n}:{d} skip')
             return answer
 
         (first_digit, other_digits) = self.splitDigits(n)
@@ -37,16 +42,24 @@ class Solution:
         for d in range(first_digit):
             # d is every number LOWER THAN first_digit
             if d in [0, 1, 2, 5, 6, 8, 9]:
+                if DEBUG: print(f'  {n}:{d} +({all_nines})')
                 answer += self.numbersContaining_0125689(all_nines)
+            else:
+                if DEBUG: print(f'  {n}:{d} skip {all_nines}')
         for d in {first_digit}:
             # d is EQUAL TO first_digit
             if d in [0, 1, 2, 5, 6, 8, 9]:
+                if DEBUG: print(f'  {n}:{d} +({other_digits})')
                 answer += self.numbersContaining_0125689(other_digits)
+            else:
+                if DEBUG: print(f'  {n}:{d} skip {other_digits}')
         return answer
 
+    @cache
     def numbersContaining_2569(self, n: int) -> int:
+        DEBUG = False
         # these are legal answers b/c they contain either 2, 5, 6, or 9
-        print(f'numbersContaining_2569({n}):')
+        if DEBUG: print(f'numbersContaining_2569({n}):')
         answer = 0
         digits = self.digitCount(n)
         if digits == 1:
@@ -54,22 +67,33 @@ class Solution:
             for d in range(first_digit + 1):
             # d is every number LESS THAN OR EQUAL TO first_digit
                 if d in [2, 5, 6, 9]:
+                    if DEBUG: print(f'  {n}:{d} +1')
                     answer += 1
+                else:
+                    if DEBUG: print(f'  {n}:{d} skip')
             return answer
         (first_digit, other_digits) = self.splitDigits(n)
         all_nines = self.maxNumberOfNDigits(digits - 1)
         for d in range(first_digit):
             # d is every number LOWER THAN first_digit
             if d in [2, 5, 6, 9]:
+                if DEBUG: print(f'  {n}:{d} +({all_nines})')
                 answer += self.numbersContaining_0125689(all_nines)
-            if d in [0, 1, 8]:
+            elif d in [0, 1, 8]:
+                if DEBUG: print(f'  {n}:{d} +({all_nines})')
                 answer += self.numbersContaining_2569(all_nines)
+            else:
+                if DEBUG: print(f'  {n}:{d} skip {all_nines}')
         for d in {first_digit}:
             # d is EQUAL TO first_digit
             if d in [2, 5, 6, 9]:
+                if DEBUG: print(f'  {n}:{d} +({other_digits})')
                 answer += self.numbersContaining_0125689(other_digits)
-            if d in [0, 1, 8]:
+            elif d in [0, 1, 8]:
+                if DEBUG: print(f'  {n}:{d} +({other_digits})')
                 answer += self.numbersContaining_2569(other_digits)
+            else:
+                if DEBUG: print(f'  {n}:{d} skip {other_digits}')
         return answer
 
     def rotatedDigits(self, n: int) -> int:
@@ -78,3 +102,6 @@ class Solution:
 # NOTE: Accepted on first Submit
 # NOTE: Runtime 39 ms Beats 94.35%
 # NOTE: Memory 16.53 MB Beats 45.22%
+# NOTE: and with cache:
+# NOTE: Runtime 29 ms Beats 99.35%
+# NOTE: Memory 17.06 MB Beats 9.13%
