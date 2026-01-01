e9f56500aee7bf1997a2b1f8ccd1fde9ac19be60
Warren Harmon <oakenraven@juno.com>
2025-12-31 19:26:29 -0500

    new leetcode and screenshot

--- python/leetcode/problems/00/66_CHALLENGE_plus_one.py
+++ python/leetcode/problems/00/66_CHALLENGE_plus_one.py
@@ -1,5 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+
         digits[-1] += 1
         while max(digits) >= 10:
             print(f"{digits=}")
@@ -13,3 +14,8 @@ class Solution:
         print(f"{digits=}")
         return digits
 
+# NOTE: Acceptance Rate 48.5% (easy)
+
+# NOTE: re-ran for challenge:
+# NOTE: Runtime 0 ms Beats 100.00%
+# NOTE: Memory 17.52 MB Beats 77.57%
