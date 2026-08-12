85f473a137f6bce06073ee12d91efc1ca31429aa
Warren Harmon <oakenraven@juno.com>
2026-08-11 21:12:58 -0400

    new leetcode

---
+++ python/leetcode/problems/29/2958_CHALLENGE_len_of_longest_subarr_w_LE_K_freq.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        
+        L = R = 0
+        max_size = 0
+        # define window === nums[L:R], therefore length === (R - L)
+        freq = Counter()
+        # print(f'start  [{L}:{R}] ({R-L})')
+        # print(f'  {freq=}')
+        # A = nums[L]
+        while L <= R < len(nums):
+            B = nums[R]
+            R += 1
+            # print(f'grow   [{L}:{R}] ({R-L})')
+            freq[B] += 1
+            # print(f'  {B=} {freq=}')
+            if freq[B] <= k:
+                size = R - L
+                # print(f'  Yes: {size=}')
+                max_size = max(size, max_size)
+                continue
+            else:
+                # print(f'  NO! {freq[B]=} > {k}')
+                while freq[B] > k:
+                    A = nums[L]
+                    L += 1
+                    freq[A] -= 1
+                    # print(f'shrink [{L}:{R}] ({R-L}) {A=}')
+                    if A == B:
+                        # print(f'  FOUND')
+                        assert freq[B] <= k
+                        break
+        return max_size
+
+# NOTE: Acceptance Rate 57.0% (medium)
+
+# NOTE: Accepted on third Run (typo, fencepost error)
+# NOTE: Accepted on second Submint (Output Exceeded)
+# NOTE: Runtime 370 ms Beats 5.19%
+# NOTE: Memory 35.42 MB Beats 15.77%
